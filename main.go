84dfd6189287aab9dfd24615a9bb310e3c44e04d
Douglas Lamb <douglaslamb@douglaslamb.com>
2016-05-22 16:46:42 -0500

    firsty

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+// alright so I want to make a thing that slurps some json and then gives you some options re sorting them like contacts. like I want to be able to look up a person by like last name, like with "kt find last-name first-name" or kt name last-name first-name or kt name last-name or kit knt knt knt knt knit knt kt knt kt I dunno.
+// so I want
+// kt name last-name
+// kt name first-name
+// kt addr address
+// kt city city (also accepts a zip)
+// kt state state
+// kt cntry country
+// kt note
+// kt email email-addrese
+// kt phone
+
+// parts of this program are
+// 1. slurping json
+// 2. filtering the json array
+// 3. printing the contents of the array to stdout
+
+func main() {
+}
